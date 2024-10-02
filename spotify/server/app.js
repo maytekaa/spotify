76a2122bee23c6504e7687cac00101ba0141b2d4
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-02 08:48:16 -0300

    conexao ao mongo- colecao artistas

--- spotify/server/app.js
+++ spotify/server/app.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import conectaNaDb from './db.js';
+import artista from './Models/Artistas.js';
 
 const app = express();
 app.use(cors());
@@ -13,6 +14,11 @@ conexao.on('error', (erro) =>  {
 conexao.once('open', () => {
     console.log('Conectado ao MongoDB')
 })
+
+app.get("/artistas", async (req, res) => {
+    const listaArtistas = await artista.find({});
+    res.status(200).json(listaArtistas)
+} )
 
 app.listen(3000, () => {
     console.log('Servidor conectado')
--- spotify/server/db.js
+++ spotify/server/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 export default async function conectaNaDb(){
-    mongoose.connect("mongodb+srv://maytekasv:FHBV5XBXdlvJOP6Y@maytekaa.x2zyu.mongodb.net/?retryWrites=true&w=majority&appName=maytekaa")
+    mongoose.connect("mongodb+srv://maytekasv:FHBV5XBXdlvJOP6Y@maytekaa.x2zyu.mongodb.net/Spotify?retryWrites=true&w=majority&appName=maytekaa")
 
     return mongoose.connection;
 }
+
